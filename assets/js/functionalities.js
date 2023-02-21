1ecfbe5e0cf823abc9931606fbc228f632c72936
shaikh_md.007 <shaikhmohammad520@gmail.com>
2023-02-21 16:40:17 +0530

    Styling updated

--- assets/js/functionalities.js
+++ assets/js/functionalities.js
@@ -1,1 +1,18 @@
 console.log('shaikh_md.')
+
+//code for Random Quotes section
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const nextButton = document.getElementById('nextButton');
+
+async function nextQuote() {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    // console.log(data);
+    nextButton.classList.add("nextQ");
+    quote.innerText = `" ${data.content} "`;
+    author.innerText = data.author;
+    author.style.color = "silver";
+    quote.style.color = "silver";
+}
+nextQuote();
