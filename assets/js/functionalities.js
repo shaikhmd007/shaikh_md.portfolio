09bae1626088a5a16c982423d1a8218b1bc79766
shaikh_md.007 <shaikhmohammad520@gmail.com>
2023-03-14 10:54:23 +0530

    styling updated

--- assets/js/functionalities.js
+++ assets/js/functionalities.js
@@ -10,9 +10,19 @@ async function nextQuote() {
     const data = await response.json();
     // console.log(data);
     nextButton.classList.add("nextQuote");
-    quote.innerText = `" ${data.content} "`;
+    quote.innerText = ` ${data.content} "`;
     author.innerText = data.author;
-    author.style.color = "white";
-    quote.style.color = "silver";
+    // author.style.color = "white";
+    // quote.style.color = "silver";
 }
 nextQuote();
+
+// The function toggles more (hidden) text when the user clicks on "Read more". The IF ELSE statement ensures that the text 'read more' and 'read less' changes interchangeably when clicked on.
+/* $('.moreless-button').click(function () {
+    $('.moretext').slideToggle();
+    if ($('.moreless-button').text() == "Read more") {
+        $(this).text("Read less")
+    } else {
+        $(this).text("Read more")
+    }
+}); */
